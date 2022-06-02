6814c0ed074382140bc4ce109a4d8d1c7515161b
sh-osama-sami <85364511+sh-osama-sami@users.noreply.github.com>
2022-06-02 18:51:01 +0200

    adduser + getuser

---
+++ GoToBus-WEB/src/service/userService.java
@@ -0,0 +1,59 @@
+package service;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import javax.ejb.*;
+import javax.inject.Inject;
+import javax.persistence.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import ejb.notification;
+import ejb.station;
+import ejb.trip;
+import ejb.user;
+
+import javax.ws.rs.*;
+
+@Stateless
+@Path("service")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class userService  implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@PersistenceContext(unitName = "hello" )
+	private EntityManager entityManager;
+	
+	@POST
+	@Path("adduser")
+	public void addUser(user u ) {
+		entityManager.persist(u);
+	}
+	
+	@GET
+	@Path("getuser/{Id}")
+	public user sayHello(@PathParam("Id") int Id)
+	{
+		return entityManager.find(user.class, Id);
+	}
+
+
+	
+
+
+}
+
