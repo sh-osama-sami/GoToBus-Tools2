80c565ac6c695e5f7baf2c27aebc3d0e0909dced
NFayed <norhan.fayed1412@gmail.com>
2022-06-02 20:47:16 +0200

    added book and search trip input classes

---
+++ GoToBus-WEB/src/service/bookInput.java
@@ -0,0 +1,19 @@
+package service;
+
+public class bookInput {
+	int user_id;
+	int trip_id;
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getTrip_id() {
+		return trip_id;
+	}
+	public void setTrip_id(int trip_id) {
+		this.trip_id = trip_id;
+	}
+	
+}
---
+++ GoToBus-WEB/src/service/searchTripInput.java
@@ -0,0 +1,33 @@
+package service;
+
+public class searchTripInput {
+	int from_station;
+	int to_station;
+	String from_date;
+	String to_date;
+	public int getFrom_station() {
+		return from_station;
+	}
+	public void setFrom_station(int from_station) {
+		this.from_station = from_station;
+	}
+	public int getTo_station() {
+		return to_station;
+	}
+	public void setTo_station(int to_station) {
+		this.to_station = to_station;
+	}
+	public String getFrom_date() {
+		return from_date;
+	}
+	public void setFrom_date(String from_date) {
+		this.from_date = from_date;
+	}
+	public String getTo_date() {
+		return to_date;
+	}
+	public void setTo_date(String to_date) {
+		this.to_date = to_date;
+	}
+	
+}
